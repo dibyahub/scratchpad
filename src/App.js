93182e4cf475d8f9dfaf2c15171bc12df94a8a66
thapakazi <milans.thapa78@gmail.com>
2021-02-19 09:50:51 +0545

    try - codemirror with react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 
-import React from "react";
+import React,{useState} from "react";
+import styled from 'styled-components';
+import {Controlled as CodeMirror} from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import './themes';
+import './modes';
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1>
-          Hello {name}
-        </h1>
-      </>
-    );
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+const Selectors = styled.div`
+  gap: 10px;
+  display: flex;
+`
+const StyledCodeMirror = styled(CodeMirror)`
+  .CodeMirror{
+      height: 95vh;
+   }
+`
+
+const Selector=({title, items,selectFunc}) => {
+  return(
+    <>
+      {title}: 
+    <select onChange={selectFunc}>
+      { items.map( item => (
+        <option key={item} value={item}>{item}</option>
+      ))}
+    </select>
+
+    </>
+  ) 
+}
+
+const welcomeMsg = `
+/* Welcome to scratchpad. Select language and theme from dropdowns */
+`
+const App = () => {
+
+  const [ html, setHtml] = useState(welcomeMsg)
+  const [ mode, setMode] = useState('javascript')
+  const [ theme, setTheme] = useState('dracula')
+  const modes = ["javascript","yaml","htmlmixed","css", "markdown"]
+  const themes = ['dracula','default', '3024-day', '3024-night', 'abcdef', 'ambiance', 'ayu-dark', 'ayu-mirage', 'base16-dark', 'base16-light', 'bespin', 'blackboard', 'cobalt', 'colorforth', 'darcula', 'duotone-dark', 'duotone-light', 'eclipse', 'elegant', 'erlang-dark', 'gruvbox-dark', 'hopscotch', 'icecoder', 'idea', 'isotope', 'lesser-dark', 'liquibyte', 'lucario', 'material', 'material-darker', 'material-palenight', 'material-ocean', 'mbo', 'mdn-like', 'midnight', 'monokai', 'moxer', 'neat', 'neo', 'night', 'nord', 'oceanic-next', 'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'rubyblue', 'seti', 'shadowfox', 'solarized dark', 'solarized light', 'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'ttcn', 'twilight', 'vibrant-ink', 'xq-dark', 'xq-light', 'yeti', 'yonce', 'zenburn', ,]
+  const selectMode =(e) => {
+    setMode(e.target.value)
   }
+  const selectTheme =(e) => {
+    setTheme(e.target.value)
+  }
+  return (
+    <StyledWrapper>
+      <Selectors>
+        <Selector title="lang" items={modes} selectFunc={selectMode}/>
+        <Selector title="theme" items={themes} selectFunc={selectTheme}/>
+      </Selectors>
+      <StyledCodeMirror
+        value={html}
+        options={{
+          mode: mode,
+          theme: theme,
+          lineNumbers: true
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setHtml(value)
+        }}
+      />
+    </StyledWrapper>
+  );
 }
 
 export default App;
---
+++ src/modes.js
@@ -0,0 +1,5 @@
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/yaml/yaml';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/markdown/markdown';
