c67a7f711f3a1e3e179dbac21b078908a1886a2d
thapakazi <milans.thapa78@gmail.com>
2021-02-19 22:47:26 +0545

    keymap - support / life without keymaps is superhard

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Controlled as CodeMirror} from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import './themes';
 import './modes';
+import './keymaps';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -40,29 +41,31 @@ const welcomeMsg = `
 `
 const App = () => {
 
-  const [ html, setHtml] = useState(welcomeMsg)
-  const [ mode, setMode] = useState('javascript')
-  const [ theme, setTheme] = useState('dracula')
-  const modes = ["javascript","yaml","htmlmixed","css", "markdown"]
-  const themes = ['dracula','default', '3024-day', '3024-night', 'abcdef', 'ambiance', 'ayu-dark', 'ayu-mirage', 'base16-dark', 'base16-light', 'bespin', 'blackboard', 'cobalt', 'colorforth', 'darcula', 'duotone-dark', 'duotone-light', 'eclipse', 'elegant', 'erlang-dark', 'gruvbox-dark', 'hopscotch', 'icecoder', 'idea', 'isotope', 'lesser-dark', 'liquibyte', 'lucario', 'material', 'material-darker', 'material-palenight', 'material-ocean', 'mbo', 'mdn-like', 'midnight', 'monokai', 'moxer', 'neat', 'neo', 'night', 'nord', 'oceanic-next', 'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'rubyblue', 'seti', 'shadowfox', 'solarized dark', 'solarized light', 'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'ttcn', 'twilight', 'vibrant-ink', 'xq-dark', 'xq-light', 'yeti', 'yonce', 'zenburn', ,]
-  const selectMode =(e) => {
-    setMode(e.target.value)
-  }
-  const selectTheme =(e) => {
-    setTheme(e.target.value)
-  }
+  const [ html, setHtml] = useState(welcomeMsg);
+  const [ mode, setMode] = useState('javascript');
+  const [keymap, setKeymap] = useState('vim');
+  const [ theme, setTheme] = useState('dracula');
+  const modes = ["javascript","yaml","htmlmixed","css", "markdown"];
+  const keymaps =  ["vim","emacs","sublime"];
+  const themes = ['dracula','default', '3024-day', '3024-night', 'abcdef', 'ambiance', 'ayu-dark', 'ayu-mirage', 'base16-dark', 'base16-light', 'bespin', 'blackboard', 'cobalt', 'colorforth', 'darcula', 'duotone-dark', 'duotone-light', 'eclipse', 'elegant', 'erlang-dark', 'gruvbox-dark', 'hopscotch', 'icecoder', 'idea', 'isotope', 'lesser-dark', 'liquibyte', 'lucario', 'material', 'material-darker', 'material-palenight', 'material-ocean', 'mbo', 'mdn-like', 'midnight', 'monokai', 'moxer', 'neat', 'neo', 'night', 'nord', 'oceanic-next', 'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'rubyblue', 'seti', 'shadowfox', 'solarized dark', 'solarized light', 'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'ttcn', 'twilight', 'vibrant-ink', 'xq-dark', 'xq-light', 'yeti', 'yonce', 'zenburn'];
+  const selectMode =(e) => setMode(e.target.value);
+  const selectTheme =(e) => setTheme(e.target.value);
+  const selectKeymap=(e) => setKeymap(e.target.value);
+
   return (
     <StyledWrapper>
       <Selectors>
         <Selector title="lang" items={modes} selectFunc={selectMode}/>
         <Selector title="theme" items={themes} selectFunc={selectTheme}/>
+        <Selector title="keymap" items={keymaps} selectFunc={selectKeymap}/>
       </Selectors>
       <StyledCodeMirror
         value={html}
         options={{
           mode: mode,
           theme: theme,
-          lineNumbers: true
+          lineNumbers: true,
+          keyMap: keymap
         }}
         onBeforeChange={(editor, data, value) => {
           setHtml(value)
---
+++ src/keymaps.js
@@ -0,0 +1,3 @@
+import 'codemirror/keymap/vim';
+import 'codemirror/keymap/emacs';
+import 'codemirror/keymap/sublime';
